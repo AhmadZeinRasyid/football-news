4a6f4a0464b5ef5cb02b22599c1096100a60b0c9
Ahmad Zein Rasyid <ahmad.zein41@ui.ac.id>
2025-09-24 14:42:10 +0700

    fix

--- football_news/settings.py
+++ football_news/settings.py
@@ -32,7 +32,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "ahmad-zein41-footballnews.pbp.cs.ui.ac.id"]
 CSRF_TRUSTED_ORIGINS = [
-    "https://ahmad-zein41-footballnews.pbp.cs.ui.ac.id/",
+    "https://ahmad-zein41-footballnews.pbp.cs.ui.ac.id",
 ]
 # Application definition
 
